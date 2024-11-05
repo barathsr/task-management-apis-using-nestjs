fdeebbd32c30146ec14ef01f6ab1e3ec2671523c
S.R.Barathkumar <barathsr67@gmail.com>
2024-11-05 23:15:25 +0530

    [Barath] Add validator for search and filter

--- src/tasks/dto/get-task-filter.dto.ts
+++ src/tasks/dto/get-task-filter.dto.ts
@@ -1,6 +1,18 @@
+import { IsEnum, IsOptional, IsString } from "class-validator";
 import { TaskStatus } from "../tasks.model";
 
 export class GetTasksFilterDto {
-    status?: TaskStatus;
-    search?: string;
+    @IsOptional()
+    @IsEnum(TaskStatus)
+    status: TaskStatus;
+
+    @IsOptional()
+    @IsString()
+    search: string;
 }
+
+// without IsOptional
+// export class GetTasksFilterDto {
+//     status?: TaskStatus;
+//     search?: string;
+// }
