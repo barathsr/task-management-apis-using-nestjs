53d6d0bd900d2f30921411eacd222ac717f9ab58
S.R.Barathkumar <barathsr67@gmail.com>
2024-11-07 16:27:28 +0530

    [Barath] Add type orm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true,
-    })
+    TypeOrmModule.forRoot(typeOrmConfig)
   ],
 })
 export class AppModule {}
