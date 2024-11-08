6e6b6abd13d1e18904ae4f90eb3e0fd85f1f3c0e
S.R.Barathkumar <barathsr67@gmail.com>
2024-11-08 16:07:23 +0530

    [Barath] Rename files

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { User } from './auth.entity';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService){}
+  constructor(private authService: AuthService) {}
 
-    @Get()
-    getUsers(): Promise<User[]>{
-        return this.authService.getUser();
-    }
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.authService.getUser();
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UserRepository } from './auth.repository';
-import { User } from './auth.entity';
+import { UserRepository } from './user.repository';
+import { User } from './user.entity';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        @InjectRepository(UserRepository)
-        private userRepository: UserRepository,
-    ){}
-    
-    async getUser(): Promise<User[]> {
-        return await this.userRepository.find()
-    }
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) {}
+
+  async getUser(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
 }
