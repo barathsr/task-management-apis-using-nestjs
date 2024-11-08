ab97798285f8a4e8d47476a231f457eccec2b469
S.R.Barathkumar <barathsr67@gmail.com>
2024-11-08 17:20:02 +0530

    [Barath] Add password and user name validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,9 +1,16 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialsDto{
-    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     userName:string;
     
-    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(32)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: "Password is weak"
+        })
     password:string;
 }
