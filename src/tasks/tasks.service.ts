f828e54b3f6f7f853e2bdc04b90485ffe17819a4
S.R.Barathkumar <barathsr67@gmail.com>
2024-11-15 23:14:55 +0530

    [Barath] Add restrictions to the users

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -37,9 +37,10 @@ export class TasksService {
     return tasks;
   }
 
-  async getTaskById (id: string): Promise<Task> {
+  async getTaskById (id: string, user: User): Promise<Task> {
     // const found = await this.tasksRepository.findOne(id);
-    const found = await this.tasksRepository.findOneBy({id});
+    // const found = await this.tasksRepository.findOneBy({id});
+    const found = await this.tasksRepository.findOne({where:{id, user}});
     if (!found){
       throw new NotFoundException(`Task with ID ${id} not found`);
     }
@@ -58,15 +59,15 @@ export class TasksService {
     return task;
   }
 
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
+  async updateTaskStatus(id: string, status: TaskStatus, user:User): Promise<Task> {
+    const task = await this.getTaskById(id, user);
     task.status = status
     await this.tasksRepository.save(task);
     return task;
   }
 
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.tasksRepository.delete({id: id});
+  async deleteTask(id: string, user:User): Promise<void> {
+    const result = await this.tasksRepository.delete({ id, user });
     if (result.affected === 0) {
       throw new NotFoundException(`Task with id: "${id}" is not found`);
     }
