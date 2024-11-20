20857c318ddcadaade45a43a68e14a4165bdb732
S.R.Barathkumar <barathsr67@gmail.com>
2024-11-20 22:16:15 +0530

    [Barath] Add env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { configValidationSchema } from './config/config.schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath:[`.env.stage.${process.env.Stage}`],
+      validationSchema: configValidationSchema,
     }),
     TasksModule,
     TypeOrmModule.forRootAsync(typeOrmConfig),
