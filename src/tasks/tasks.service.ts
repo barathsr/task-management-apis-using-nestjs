813e0a43e976e865643ff16d34646c60b9e14a3a
S.R.Barathkumar <barathsr67@gmail.com>
2024-11-21 15:04:46 +0530

    [Barath] Change path in all the imports to resolve the bug in NestJs and Jest

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { User } from 'src/auth/user.entity';
+import { User } from '../auth/user.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { TaskStatus } from './tasks.status.enum';
